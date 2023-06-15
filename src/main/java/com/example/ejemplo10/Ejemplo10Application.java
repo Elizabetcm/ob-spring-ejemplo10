0f8e5befceebea8a50ef6ef09881c12eaef0b5e5
Elizabeth <elizabetcolquem@gmail.com>
2023-06-14 22:17:06 -0400

    spring heroku app config

---
+++ src/main/java/com/example/ejemplo10/Ejemplo10Application.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo10Application.class, args);
+	}
+
+}
